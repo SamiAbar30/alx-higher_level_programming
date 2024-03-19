da3a40f8a9b703209dfb688b440ea77b7ff5b6c4
SamiAbar30 <samiabar30@gmail.com>
2024-03-19 16:57:44 -0700

    fix style code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     tmp_list = my_list[:]
     if 0 <= idx < len(my_list):
         tmp_list[idx] = element
-        return(tmp_list)
+        return tmp_list
 
-    return(my_list)
+    return my_list
