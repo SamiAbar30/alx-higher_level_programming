2c47306cb704c7c12bc8d0b3e6eae22b6e966fb1
SamiAbar30 <samiabar30@gmail.com>
2024-03-19 17:03:18 -0700

    fix style code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if i is not 'c' and i is not 'C':
             new_str += i
     return new_stri
-# return my_string.translate({ord(c): None for c in "cC"})
